e4f03e34d47ea43d6e1ccef7d4b867424f42cc16
Skyoohn <135787762+Skyoohn@users.noreply.github.com>
2023-12-24 16:16:15 +0900

    이거임

---
+++ BS_THSManager/src/main/java/com/example/thsmanager/service/TorihikisakiMainService.java
@@ -0,0 +1,36 @@
+package com.example.thsmanager.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.thsmanager.entity.TorihikisakiMain;
+import com.example.thsmanager.mapper.TorihikisakiMainMapper;
+
+@Service
+@Transactional
+public class TorihikisakiMainService {
+	
+	@Autowired
+	private TorihikisakiMainMapper torihikisakiMainMapper;
+	
+	public List<TorihikisakiMain> selectTorihikisakiMains(){
+		List<TorihikisakiMain> torihikisakiMains = torihikisakiMainMapper.selectTorihikisakiMains();
+		return torihikisakiMains;
+	}
+	public TorihikisakiMain selectTorihikisakiMain(Integer torihikiId) {
+		return torihikisakiMainMapper.selectTorihikisakiMain(torihikiId);
+	}
+	public void addTorihikisakiMain(TorihikisakiMain torihikisakiMain) {
+		torihikisakiMainMapper.addTorihikisakiMain(torihikisakiMain);
+	}
+	public void updateTorihikisakiMain(TorihikisakiMain torihikisakiMain) {
+		torihikisakiMainMapper.updateTorihikisakiMain(torihikisakiMain);
+	}
+	public void delTorihikisakiMain(Integer torihikiId) {
+		torihikisakiMainMapper.delTorihikisakiMain(torihikiId);
+	}
+
+}
